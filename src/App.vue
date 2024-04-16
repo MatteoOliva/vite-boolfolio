8204e5b10d33b220f9bc045ed1599b3ef849acfa
MatteoOliva <oliva.matteo98@outlook.com>
2024-04-16 15:39:03 +0200

    axios installato

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script>
+import axios from 'axios';
+
 export default {
   data() {
-    return {title:"frontend"}
-  }
-}
+    return {title:"frontend"};
+  },
 
+  // appena l'app e' logicamente pronta
+  created() {
+    console.log(axios);
+  },
+};
 </script>
+
+
 <template>
+  <div class="container mt-5"></div>
 
   <h1>{{ title }}</h1>
 </template>
