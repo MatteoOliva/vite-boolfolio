b035e948415afceb5b72f6530071dc3bbda07f22
MatteoOliva <oliva.matteo98@outlook.com>
2024-04-16 16:04:23 +0200

    feat : milestone 4

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,35 @@ import axios from 'axios';
 
 export default {
   data() {
-    return {title:"frontend"};
+    return {title:"frontend",
+    projects: [],
+  };
   },
 
   // appena l'app e' logicamente pronta
   created() {
-    console.log(axios);
+    axios.get('http://127.0.0.1:8000/api/projects').then((response)=>{
+      this.projects = response.data.data;
+      console.log(response.data.data);
+    })
   },
 };
 </script>
 
 
 <template>
-  <div class="container mt-5"></div>
+  <div class="container mt-5">
+    <h1>{{ title }}</h1>
+    <div v-for="project in projects">
+      <ul><strong>Numero Progetto:</strong> {{ project.id }}</ul>
+      <ul><strong>Title:</strong> {{ project.title }}</ul>
+      <ul><strong>Content:</strong> {{ project.content }}</ul>
+      <ul><strong>Type:</strong> {{ project.type.label }}</ul>
+      <hr>
+    </div>
 
-  <h1>{{ title }}</h1>
+  </div>
+
 </template>
 
 <style lang="scss">
